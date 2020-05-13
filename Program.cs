51c9a382c447758ba7e31da3bd7fcc4da8c7bf1f
bradwellsb <support@wellsb.com>
2020-05-12 23:09:20 -0500

    Enable SSL by default

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace StatiqBlog
         static async Task<int> Main(string[] args) =>
             await Bootstrapper.Factory
                 .CreateWeb(args)
+                .AddSetting(Keys.Host, new Uri(Constants.SiteUri).Host)
+                .AddSetting(Keys.LinksUseHttps, true)
                 .AddSetting(
                         Keys.DestinationPath,
                         Config.FromDocument((doc, ctx) =>
