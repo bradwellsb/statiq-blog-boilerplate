29faeeb01916e218f4306a28b10537534c560035
bradwellsb <support@wellsb.com>
2020-05-15 21:09:31 -0500

    Add posts per page control

--- Constants.cs
+++ Constants.cs
@@ -10,5 +10,6 @@ namespace StatiqBlog
         public const string SiteUri = "https://example.com";
         public const string BlogTitle = "Your Blog";
         public const string BlogPath = "blog"; // ex. "blog" indicates https://example.com/blog/
+        public const int PostsPerPage = 10;
     }
 }
